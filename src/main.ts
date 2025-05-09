8a39fe534ee088ce865f49e4d871ceb9ac05f50a
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-09 22:09:50 +0000

    feat: Add Swagger support for API documentation
    Integrate @nestjs/swagger to enable API documentation for the Movie Reservation System. This includes setting up the Swagger module and defining the API's metadata.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Movie Reservation System')
+    .setDescription('A system that allows users to reserve movie tickets')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_controllerKey: string, methodKey: string) =>
+      methodKey,
+  };
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api/documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 

d48ef90b09a64f1c8a561bb78588ba25c4896252
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-10 16:22:32 +0000

    feat(showtimes): Set up showtime endpoints
    Implement Showtimes module with controller and service for managing showtimes.
    Includes DTOs for creating and updating showtimes, along with tests.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { LoggingMiddleware } from './logging/logging.middleware';
 import { MoviesModule } from './movies/movies.module';
+import { ShowtimesModule } from './showtimes/showtimes.module';
 
 @Module({
-  imports: [AuthModule, MoviesModule],
+  imports: [AuthModule, MoviesModule, ShowtimesModule],
   controllers: [],
   providers: [PrismaService],
 })

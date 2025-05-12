cc30d07e573f3317ead4203ccbca5b703b656568
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-12 08:07:53 +0000

    feat(seats): Implement CreateSeatDto with validation rules

--- src/seats/dto/create-seat.dto.ts
+++ src/seats/dto/create-seat.dto.ts
@@ -1,1 +1,20 @@
-export class CreateSeatDto {}
+import { IsDefined, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class CreateSeatDto {
+  /**
+   * Seat's label
+   * @example 'A1'
+   *
+   */
+  @IsNotEmpty()
+  @IsString()
+  label: string;
+
+  /**
+   * Seat's auditorium ID
+   * @example '123e4567-e89b-12d3-a456-426655440000'
+   */
+  @IsDefined()
+  @IsUUID()
+  auditoriumId: string;
+}

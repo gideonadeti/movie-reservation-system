6bf37d126f68fbcc0583e5cdad06b85f752080e2
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-12 07:11:43 +0000

    feat(auditoriums): Add capacity field to Auditorium model

---
+++ prisma/migrations/20250512070433_add_capacity_field_to_auditorium_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Auditorium" ADD COLUMN     "capacity" INTEGER NOT NULL DEFAULT 0;
---
+++ prisma/migrations/20250512070603_remove_default_from_capacity_field_of_auditorium_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Auditorium" ALTER COLUMN "capacity" DROP DEFAULT;

1f527e9374e232118b19551fd1251dae407f74c8
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-12 07:13:19 +0000

    feat(auditoriums): Implement CreateAuditoriumDto with validation

--- src/auditoriums/dto/create-auditorium.dto.ts
+++ src/auditoriums/dto/create-auditorium.dto.ts
@@ -1,1 +1,26 @@
-export class CreateAuditoriumDto {}
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateAuditoriumDto {
+  /**
+   * Auditorium's name
+   * @example 'Auditorium 1'
+   */
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  /**
+   * Auditorium's capacity
+   * @example 100
+   */
+  @IsDefined()
+  @IsNumber()
+  @IsPositive()
+  capacity: number;
+}

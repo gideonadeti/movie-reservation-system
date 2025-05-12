bb0574d3950f5bd63f0727a846dd251c179da6a6
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-12 07:20:47 +0000

    feat(auditoriums): Add PrismaService to module providers

--- src/auditoriums/auditoriums.module.ts
+++ src/auditoriums/auditoriums.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AuditoriumsService } from './auditoriums.service';
 import { AuditoriumsController } from './auditoriums.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [AuditoriumsController],
-  providers: [AuditoriumsService],
+  providers: [AuditoriumsService, PrismaService],
 })
 export class AuditoriumsModule {}

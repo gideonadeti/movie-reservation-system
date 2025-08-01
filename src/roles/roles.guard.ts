a5d82f0b2b15437d9ba83444c39dd955fd43bd95
Gideon Adeti <gideonadeti0@gmail.com>
2025-08-01 10:57:05 +0000

    feat(roles): Add public access check in RolesGuard

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -4,12 +4,22 @@ import { Request } from 'express';
 
 import { Role, User } from 'generated/prisma';
 import { ROLES_KEY } from './roles.decorator';
+import { IS_PUBLIC_KEY } from 'src/auth/public.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),

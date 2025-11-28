6d0c73d91afd81118559665c15c24d2f74049d19
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-28 05:35:59 +0000

    feat(prisma): Configure PrismaClient logging and error format
    Add a constructor to the PrismaService to set logging options based on the environment and specify a minimal error format for improved clarity in production.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,14 @@ import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log:
+        process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+      errorFormat: 'minimal',
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }

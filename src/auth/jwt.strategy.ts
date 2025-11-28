e4c76047851a34e9d338e7db9a1c9d1b73b2e13c
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-28 14:53:45 +0000

    refactor(auth): Update JWT strategies to use ConfigService for secret management
    Modified JwtStrategy and RefreshJwtStrategy to utilize ConfigService for retrieving JWT secrets from environment variables, enhancing security and maintainability by removing hardcoded values.

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
-import { jwtConstants } from './jwt.constants';
 import { Role } from '@prisma/client';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: jwtConstants.accessSecret as string,
+      secretOrKey: configService.get('JWT_ACCESS_SECRET') as string,
     });
   }
 

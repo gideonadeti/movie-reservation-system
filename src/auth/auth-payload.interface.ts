d74fac467c4743dfac58d7fe3d89dc5d3dcc7614
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-29 10:48:55 +0000

    feat(auth): Update AuthPayload interface to include name, createdAt, and updatedAt fields
    Enhanced the AuthPayload interface by adding the name, createdAt, and updatedAt properties, improving the structure and detail of authentication payloads for better user data management.

--- src/auth/auth-payload.interface.ts
+++ src/auth/auth-payload.interface.ts
@@ -1,8 +1,11 @@
 import { Role } from '@prisma/client';
 
 export interface AuthPayload {
-  email: string;
   sub: string;
+  name: string;
+  email: string;
   role: Role;
+  createdAt: Date;
+  updatedAt: Date;
   jti: string;
 }

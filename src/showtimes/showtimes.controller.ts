7643dbd92a2c69dc2f2899a77e6065b33e8455a9
Gideon Adeti <gideonadeti0@gmail.com>
2025-12-01 17:34:46 +0000

    feat(showtimes): Add seed endpoint for showtimes data
    Implemented a new POST endpoint in ShowtimesController to seed showtimes data using the SeedShowtimesDto. This addition allows for flexible creation of showtimes during the seeding process, enhancing the functionality of the showtimes management.

--- src/showtimes/showtimes.controller.ts
+++ src/showtimes/showtimes.controller.ts
@@ -13,15 +13,15 @@ import {
 import { ShowtimesService } from './showtimes.service';
 import { CreateShowtimeDto } from './dto/create-showtime.dto';
 import { UpdateShowtimeDto } from './dto/update-showtime.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RolesGuard } from 'src/roles/roles.guard';
 import { Roles } from 'src/roles/roles.decorator';
 import { UserRole } from '@prisma/client';
 import { FindAllShowtimesDto } from './dto/find-all-showtimes.dto';
 import { Public } from 'src/auth/public.decorator';
+import { SeedShowtimesDto } from './dto/seed-showtimes.dto';
 
-@ApiTags('Showtimes')
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles(UserRole.ADMIN)
@@ -32,6 +32,11 @@ export class ShowtimesController {
   @Post()
   create(@Body() createShowtimeDto: CreateShowtimeDto) {
     return this.showtimesService.create(createShowtimeDto);
+  }
+
+  @Post('seed')
+  seed(@Body() dto: SeedShowtimesDto) {
+    return this.showtimesService.seed(dto.count);
   }
 
   @Get()
